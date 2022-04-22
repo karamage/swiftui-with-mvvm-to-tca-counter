765b2bd4c31def085a9ac791a828851cb814cdf2
karamage <karamage.umauma@gmail.com>
2022-04-22 15:20:37 +0900

    Initial Commit

---
+++ Shared/swiftui_with_mvvm_to_tca_counterApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_with_mvvm_to_tca_counterApp.swift
+//  Shared
+//
+//  Created by Masaaki Kakimoto on 2022/04/22.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_with_mvvm_to_tca_counterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
